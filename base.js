f8cdc93a2d7f655e59cabe1aacc6ed0cb49ef6b4
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-09 14:41:35 -0700

    added basic functionality

--- base.js
+++ base.js
@@ -2,6 +2,16 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+var blogText;
+  
+
+
+ $('#blogPost').submit(function(e) {
+ 	e.preventDefault();
+ 	blogText = $('#blogText').val();
+ 	$('#blogText').val("");
+ 	$('#allPosts').append("<textarea>" + blogText + "</textarea>" + "<hr>");
+ });
 
 });
+// 
