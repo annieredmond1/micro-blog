09dfc24d2fd8fb178125ad7efa9acad6e24d80a0
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-12 13:19:59 -0700

    updated js so that comments are being pushed into the post object

--- base.js
+++ base.js
@@ -18,6 +18,8 @@ var commentButton;
 var commentInput;
 var commentDiv;
 var postNumber;
+var newPost;
+	
 
   //what to do when form is submitted
  $('#blogPost').submit(function(e) {
@@ -29,47 +31,51 @@ var postNumber;
  	//variable for the time
  	var blogTime = new Date();
 
- 	//create new obj with the post
- 	new Post(blogText);
+ 	
 
  	//variable for the comment input form
  	commentInput = "<form type='text' class='commentInput'><input type='text' class='form-control commentText' placeholder='Type your comment here...'>";
  	// //variable for the comment submit button
  	commentButton = "<button type='submit' class='btn-group-xs pull-right'>Add Comment</button></form>";
  	//variable for the div to hold the comments
- 	commentDiv = "<div class='col-md-8 col-md-offset-2 allComments'></div>";
+ 	commentDiv = "<div class='col-xs-8 col-xs-offset-2 allComments'></div>";
 
  	//append the new post to the page with a time stamp and comment button if something is written
  	if($('#blogText').val()) {
  		//add one to the count
  		postCount++;
  		postNumber = postCount;
+ 		//create new obj with the post
+ 		newPost = new Post(blogText);
  		$('#allPosts').prepend("<div><div class='specificPost'><form class='blogPosts'>" + "<font size='4'><big><strong>Post " + postCount + ":<br></strong></big>  " + blogText + "<br><em>Posted at:  " + blogTime + "</em></div><br><big><font color='#C6D4F1'>Comments:</font></big><br></font></form>" + commentDiv + commentInput + commentButton + "<hr></div>");
- 		
- 		console.log(blogText + " " + blogTime);
  	}
  	//reset the form to blank
  	$('#blogText').val("");
  	
  	addCommentSubmitHandler();
-
+ 	
+ 	
+ 	
  });
 
 
 var commentBody;
 
-//  //What to do when comment button is clicked
+// create function for when comments are submitted
  function addCommentSubmitHandler() {
  	$('.commentInput').submit(function(e) {
  	e.preventDefault();
+ 	//get content of the comment
   	commentBody = $(this).parent().find('.commentText').val();
- 	 	console.log(this);
- 	 // $(this).parent().find('.allComments').append(commentBody +"<br>");
+ 	//if there's a value in the comment form, append it to the page
  	if(commentBody) {
  		$(this).parent().find('.allComments').append("<div class='specificComment'><font size='3'>" + commentBody + "</font></div><br><br><br>");
+ 		
  	}
- 	// Post.comments.push(commentBody);
- 	console.log("hello" + commentBody);
+ 	//push new comment into the post object
+ 	newPost.comments.push(commentBody);
+ 	console.log(newPost);
+ 	//clear comment input form
  	$('.commentText').val("");
  });
  }
